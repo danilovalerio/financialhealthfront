7aae19b9ad2e894854f3b88a1c79f70778b039a3
danilovalerio <danilo_vs@hotmail.com>
2023-05-19 10:51:27 -0300

    Logo atualizado para teste

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,7 @@
 
         <!-- Icon -->
         <div class="fadeIn first">
-            <img src="http://cdn.onlinewebfonts.com/svg/img_568656.png" id="icon" alt="User Icon" />
+            <img src="../assets/logo_enter.png" id="icon" alt="User Icon" />
         </div>
 
         <!-- Login Form -->
